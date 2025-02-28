6a6a32b41a23180a778c0dc463db635247135483
Richard Davis <rich@davisfamily.online>
2025-02-28 09:56:13 +0000

    Initial commit - Spring Boot RabbitMQ & MongoDB setup

---
+++ src/main/java/com/boots/message_store_poc/MessageStorePocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boots.message_store_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStorePocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStorePocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boots/message_store_poc/MessageStorePocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boots.message_store_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageStorePocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
